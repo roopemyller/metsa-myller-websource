e7b6883a0f3685b8f68693d9794e342569203164
Roope Myller <rmyller@archlinux.localdomain>
2025-03-17 16:10:48 +0200

    header minor fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Box, AppBar, Toolbar, Typography, Button,  } from "@mui/material"
+import { Box, AppBar, Toolbar, Typography, Button, Container,  } from "@mui/material"
 import { Link } from "react-router-dom"
 
 
@@ -6,19 +6,21 @@ const Header = () => {
     return (
         <Box>
             <AppBar position="fixed" color="primary">
-                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Box sx = {{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 1 }}>
-                        <img src="logo.png" alt="logo" style={{width: 75, height: 75, padding: 2}}/>
-                    </Box>
-                    <Typography variant="h4" component="div" sx={{ color: 'secondary.main' }}>Metsä - Myller</Typography>
-                    
-                    <Box sx = {{ justifyContent: "left",display: 'flex', gap: 1 }}>
-                        <Button variant="outlined" color="inherit" component={Link} to="/">Etusivu</Button>
-                        <Button variant="outlined" color="inherit" component={Link} to="/">Meistä</Button>
-                        <Button variant="outlined" color="inherit" component={Link} to="/">Palvelut</Button>
-                        <Button variant="outlined" color="inherit" component={Link} to="/">Yhteystiedot</Button>
-                    </Box>
-                </Toolbar>
+                <Container maxWidth="lg">
+                    <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <Box sx = {{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 1 }}>
+                            <img src="logo.png" alt="logo" style={{width: 75, height: 75, padding: 2}}/>
+                        </Box>
+                        <Typography variant="h4" component="div" sx={{ color: 'secondary.main' }}>Metsä - Myller</Typography>
+                        
+                        <Box sx = {{ justifyContent: "left",display: 'flex', gap: 1 }}>
+                            <Button variant="outlined" color="inherit" component={Link} to="/">Etusivu</Button>
+                            <Button variant="outlined" color="inherit" component={Link} to="/">Meistä</Button>
+                            <Button variant="outlined" color="inherit" component={Link} to="/">Palvelut</Button>
+                            <Button variant="outlined" color="inherit" component={Link} to="/">Yhteystiedot</Button>
+                        </Box>
+                    </Toolbar>
+                </Container>
             </AppBar>
         </Box>
     )
