e6f62c50851219bd7377c3493e6fbee8df2af0c1
Roope Myller <roope.myller@gmail.com>
2025-04-06 18:35:58 +0300

    added some content to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -13,15 +13,34 @@ const Home : React.FC = () => {
         >
             <Box sx={{ flexGrow: 1, justifyContent: { xs: "center", md: "left" } }}>
                 <Typography variant="h2" sx={{wordBreak: "break-word", fontSize: { xs: '2.5rem', md: '3.5rem' }}}>METSÄ-MYLLER palveluksessasi!</Typography>
-                <Typography variant="body1">
-                    Tarvitsetko apua metsätilasi hoidossa? <br/>
-                    Eikö terassilta näe järvelle? <br/>
-                    Onko pihallasi liikaa puita tai vaarallisia puita? <br/>
-                    Haittaako pihalla oleva kanto liikkumista tai ruohonleikkuuta? <br/>
-                    Ammottaako halkoliiteri tyhjyyttä?
+                <Typography variant="body1" sx={{ fontSize: { xs: '1rem', md: '1.3rem' } }}>
+                    <ul>Tarvitsetko apua metsätilasi hoidossa?</ul>
+                    <ul>Eikö terassilta näe järvelle?</ul>
+                    <ul>Onko pihallasi liikaa puita tai vaarallisia puita?</ul>
+                    <ul>Haittaako pihalla oleva kanto liikkumista tai ruohonleikkuuta?</ul>
+                    <ul>Ammottaako halkoliiteri tyhjyyttä?</ul>
+                </Typography>
+                <Typography variant="h5" sx={{ marginTop: 2 }}>
+                    Metsätyö Petri Myller toteuttaa kaikki metsätyöt ja puunkaadot ammattitaidolla ja luotettavasti!
                 </Typography>
                 <br />
                 <Button variant="outlined" color="primary" component={Link} to="/yhteystiedot">Ota yhteyttä!</Button>            
+                <br />
+                <Box
+                    component="img"
+                    src="./yleis-2.jpg"
+                    alt="Metsä-Myller Yleiskuva"
+                    sx={{
+                        display: { xs: "none", md: "block" },
+                        marginTop: 2,
+                        width: '100%',
+                        maxWidth: '650px',
+                        height: 'auto',
+                        objectFit: 'contain',
+                        borderRadius: '8px',
+                        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.5)',
+                    }}
+                />
             </Box>
             <Box
                 display={{ xs: "none", md: "block" }}
@@ -30,7 +49,7 @@ const Home : React.FC = () => {
                 alt="Metsä-Myller henkilökuva"
                 sx={{
                     paddingRight: 15,
-                    maxHeight: 800,
+                    maxHeight: 850,
                     width: 'auto',
                     objectFit: 'contain',
                 }}
@@ -41,7 +60,7 @@ const Home : React.FC = () => {
                 src={"./henkilo-1.jpg"}
                 alt="Metsä-Myller henkilökuva"
                 sx={{
-                    maxHeight: 400,
+                    maxHeight: 550,
                     width: 'auto',
                     objectFit: 'contain',
                     borderRadius: '8px',
